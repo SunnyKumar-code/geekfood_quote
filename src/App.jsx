e9b43df1c132c5f24167d796c6090f110680f8e6
Sunny Kumar <sk202542@gmail.com>
2024-12-20 12:44:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Footer from "./components/Footer/footer"
+import Header from "./components/Header/header"
+import Hero from "./components/heroSection/hero"
+import Main from "./components/main/main"
+import Main2 from "./components/main/main2"
+import Quote from "./quote/quote"
+function App() {
+return (<div>
+        <Header/>
+        {/* <Hero/>
+        <Main/>
+        <Main2/> */}
+        <Quote/>
+        <Footer/>
+</div>
+)}
+
+export default App
